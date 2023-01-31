4fb23ae3724de91dda375319d1620c430f9ce87a
midhunps <midhunps.0@gmail.com>
2023-01-31 15:25:19 +0530

    widtch corrected

--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -19,11 +19,11 @@
                 </div>
             </form>
         </div>
-        <div class="flex flex-col">
+        <div class="w-4/5 flex flex-col">
             <div>
                 <h3 class="text-center font-bold underline text-xl my-1">Member details</h3>
             </div>
-        <div class="my-2 w-4/5 flex flex-wrap space-x-2 justify-evenly items-stretch">
+        <div class="my-2 flex flex-wrap space-x-2 justify-evenly items-stretch">
             <div class="w-2/5 min-w-fit p-1 border border-gray-300 rounded-md">
                 <h3 class="text-center font-bold underline text-lg my-1">Personal details</h3>
                 <table class="w-full min-w-[350px] m-auto max-w-lg border border-gray-400 rounded-md overflow-hidden">
