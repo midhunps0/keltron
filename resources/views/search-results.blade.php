002c86a5a48b3180279017f9190a9fc92c7f4d58
midhunps <midhunps.0@gmail.com>
2023-01-31 17:57:32 +0530

    Empty records message

--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -22,8 +22,7 @@
                 let p = {};
                 p.search = this.regno;
                 axios.get(
-                    '{{route('search')}}',
-                    {
+                    '{{ route('search') }}', {
                         params: p
                     }
                 ).then((r) => {
@@ -55,92 +54,98 @@
             </form>
         </div>
         <div id="resultsdiv" class=" relative w-4/5 flex flex-col">
-        @fragment('results')
-            <button type="button"
-                class="bg-gray-500 hover:bg-gray-300 text-white hover:text-gray-600 px-2 py-1 rounded-md shadow-md absolute top-2 right-2"
-                @click.stop.prevent="window.print()">Print</button>
-            <div>
-                <h3 class="text-center font-bold underline text-xl my-1">Member details</h3>
-            </div>
             @if (isset($person) && isset($amounts))
-            <div class="my-2 flex flex-wrap space-x-2 justify-evenly items-stretch">
-                <div class="w-2/5 min-w-fit p-1 border border-gray-300 rounded-md">
-                    <h3 class="text-center font-bold underline text-lg my-1">Personal details</h3>
-                    <table
-                        class="w-full min-w-[350px] m-auto max-w-lg border border-gray-400 rounded-md overflow-hidden">
-                        <tr>
-                            <th class="w-1/2 py-1 px-2">Particulars</th>
-                            <th class="w-1/2 py-1 px-2">Value</th>
-                        </tr>
+                @fragment('results')
+                    <button type="button"
+                        class="bg-gray-500 hover:bg-gray-300 text-white hover:text-gray-600 px-2 py-1 rounded-md shadow-md absolute top-2 right-2"
+                        @click.stop.prevent="window.print()">Print</button>
+                    <div>
+                        <h3 class="text-center font-bold underline text-xl my-1">Member details</h3>
+                    </div>
+                    <div class="my-2 flex flex-wrap space-x-2 justify-evenly items-stretch">
+                        <div class="w-2/5 min-w-fit p-1 border border-gray-300 rounded-md">
+                            <h3 class="text-center font-bold underline text-lg my-1">Personal details</h3>
+                            <table
+                                class="w-full min-w-[350px] m-auto max-w-lg border border-gray-400 rounded-md overflow-hidden">
+                                <tr>
+                                    <th class="w-1/2 py-1 px-2">Particulars</th>
+                                    <th class="w-1/2 py-1 px-2">Value</th>
+                                </tr>
 
-                        <tr>
-                            <td class="py-1 px-2">Reg. No.</td>
-                            <td class="py-1 px-2">{{ $person->regno }}</td>
-                        </tr>
-                        <tr>
-                            <td class="py-1 px-2">Name</td>
-                            <td class="py-1 px-2">{{ $person->empname }}</td>
-                        </tr>
-                        <tr>
-                            <td class="py-1 px-2">Guardian Name</td>
-                            <td class="py-1 px-2">{{ $person->guardianname }}</td>
-                        </tr>
-                        <tr>
-                            <td class="py-1 px-2">Address</td>
-                            <td class="py-1 px-2">{{ $person->empcuraddress }}</td>
-                        </tr>
-                        <tr>
-                            <td class="py-1 px-2">DOB</td>
-                            <td class="py-1 px-2">{{ $person->dateofbirth }}</td>
-                        </tr>
-                        <tr>
-                            <td class="py-1 px-2">Start Date</td>
-                            <td class="py-1 px-2">{{ $person->startingdate }}</td>
-                        </tr>
-                        <tr>
-                            <td class="py-1 px-2">Reg. Date</td>
-                            <td class="py-1 px-2">{{ $person->regdate }}</td>
-                        </tr>
-                        <tr>
-                            <td class="py-1 px-2">Create Date</td>
-                            <td class="py-1 px-2">{{ $person->createdate }}</td>
-                        </tr>
-                        <tr>
-                            <td class="py-1 px-2">ID Mark 1</td>
-                            <td class="py-1 px-2">{{ $person->idmark1 }}</td>
-                        </tr>
-                        <tr>
-                            <td class="py-1 px-2">ID Mark 2</td>
-                            <td class="py-1 px-2">{{ $person->idmark2 }}</td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="w-2/5 min-w-fit my-2 p-1 border border-gray-300 rounded-md">
-                    <h3 class="text-center font-bold underline text-lg my-1 ">Transaction details</h3>
-                    <table class="w-full min-w-[350px] m-auto max-w-lg overflow-hidden">
-                        <tr>
-                            <th class="py-1 px-2 text-bold">Date</th>
-                            <th class="py-1 px-2 text-bold">Voucher No.</th>
-                            <th class="py-1 px-2 text-bold">Amount</th>
-                            <th class="py-1 px-2 text-bold">Tenure</th>
-                        </tr>
-                        @foreach ($amounts as $amt)
-                            <tr>
-                                <td class="py-1 px-2">{{ $amt->transdate }}</td>
-                                <td class="py-1 px-2">{{ $amt->vouchernum }}</td>
-                                <td class="py-1 px-2">{{ $amt->amount }}</td>
-                                <td class="py-1 px-2">{{ $amt->remark }}</td>
-                            </tr>
-                        @endforeach
-                    </table>
-                </div>
-            </div>
-            @else
-            <div class="my-2 flex flex-wrap space-x-2 justify-evenly items-stretch">
-                <h3 class="text-center font-bold underline text-lg my-1 text-red-400">No details found.</h3>
-            </div>
+                                <tr>
+                                    <td class="py-1 px-2">Reg. No.</td>
+                                    <td class="py-1 px-2">{{ $person->regno }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-1 px-2">Name</td>
+                                    <td class="py-1 px-2">{{ $person->empname }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-1 px-2">Guardian Name</td>
+                                    <td class="py-1 px-2">{{ $person->guardianname }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-1 px-2">Address</td>
+                                    <td class="py-1 px-2">{{ $person->empcuraddress }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-1 px-2">DOB</td>
+                                    <td class="py-1 px-2">{{ $person->dateofbirth }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-1 px-2">Start Date</td>
+                                    <td class="py-1 px-2">{{ $person->startingdate }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-1 px-2">Reg. Date</td>
+                                    <td class="py-1 px-2">{{ $person->regdate }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-1 px-2">Create Date</td>
+                                    <td class="py-1 px-2">{{ $person->createdate }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-1 px-2">ID Mark 1</td>
+                                    <td class="py-1 px-2">{{ $person->idmark1 }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-1 px-2">ID Mark 2</td>
+                                    <td class="py-1 px-2">{{ $person->idmark2 }}</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="w-2/5 min-w-fit my-2 p-1 border border-gray-300 rounded-md">
+                            <h3 class="text-center font-bold underline text-lg my-1 ">Transaction details</h3>
+                            {{-- @if (count($amounts) > 0) --}}
+                            <table class="w-full min-w-[350px] m-auto max-w-lg overflow-hidden">
+                                <tr>
+                                    <th class="py-1 px-2 text-bold">Date</th>
+                                    <th class="py-1 px-2 text-bold">Voucher No.</th>
+                                    <th class="py-1 px-2 text-bold">Amount</th>
+                                    <th class="py-1 px-2 text-bold">Tenure</th>
+                                </tr>
+                                @foreach ($amounts as $amt)
+                                    <tr>
+                                        <td class="py-1 px-2">{{ $amt->transdate }}</td>
+                                        <td class="py-1 px-2">{{ $amt->vouchernum }}</td>
+                                        <td class="py-1 px-2">{{ $amt->amount }}</td>
+                                        <td class="py-1 px-2">{{ $amt->remark }}</td>
+                                    </tr>
+                                @else
+                                <tr>
+                                    <td colspan="4">No transactions recorded for this user.</td>
+                                </tr>
+                                @endforeach
+                            </table>
+                            {{-- @endif --}}
+                        </div>
+                    </div>
+                @endfragment
+            {{-- @else
+                <div class="my-2 flex flex-wrap space-x-2 justify-evenly items-stretch">
+                    <h3 class="text-center font-bold underline text-lg my-1 text-red-400">No details found.</h3>
+                </div> --}}
             @endif
-        @endfragment
         </div>
     </div>
 
