f383598380ca9f0faadd4f4888bb7541386ca198
midhunps <midhunps.0@gmail.com>
2023-01-31 13:10:55 +0530

    object_vars

---
+++ resources/views/search-results.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <table>
+        <tr>
+            <th>Particulars</th>
+            <th>Value</th>
+        </tr>
+        <tr>
+            <td>Reg. No.</td>
+            <td></td>
+        </tr>
+    </table>
+</div>
