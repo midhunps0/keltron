14e8845a817197fcbc24f80159e059b6ec2cd777
midhunps <midhunps.0@gmail.com>
2023-01-31 12:24:12 +0530

    removed test ok

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -14,7 +14,6 @@ class DefaultController extends Controller
 
     public function search()
     {
-        return 'ok';
         $result = DB::unprepared(
             "SELECT regno, empname, vouchernum, transdate, amount, remark FROM public.registration JOIN financialaccounting.ledger ON public.registration.regid = financialaccounting.ledger.regid Join public.remarks ON financialaccounting.ledger.remarks = public.remarks.remindex Where regno='8/05/546/100'"
         )->get();
