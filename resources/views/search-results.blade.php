dae248294640949581f0dafc2e5d629fc43a4f06
midhunps <midhunps.0@gmail.com>
2023-01-31 14:21:48 +0530

    app.css direct link

--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Search</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="app.css">
 </head>
 <body>
     <div>
