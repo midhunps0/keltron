2fc693a378b1f09f2d1236ef29037b26a554acf2
midhunps <midhunps.0@gmail.com>
2023-01-31 13:24:36 +0530

    With view

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -18,14 +18,14 @@ class DefaultController extends Controller
         //     "SELECT regno, empname, guardianname, empcuraddress, dateofbirth, startingdate, regdate, public.registration.createdate, idmark1, idmark2, vouchernum, transdate, amount, remark FROM public.registration JOIN financialaccounting.ledger ON public.registration.regid = financialaccounting.ledger.regid Join public.remarks ON financialaccounting.ledger.remarks = public.remarks.remindex Where regno='8/05/546/100'"
         // );
         $personResults = DB::select(
-            "SELECT * FROM public.registration Where regno='8/05/546/100'"
+            "SELECT regno, empname, guardianname, empcuraddress, dateofbirth, startingdate, regdate, public.registration.createdate, idmark1, idmark2 FROM public.registration Where regno='8/05/546/100'"
         );
         $amountResults = DB::select(
             "SELECT financialaccounting.ledger.transdate FROM public.registration JOIN financialaccounting.ledger ON public.registration.regid = financialaccounting.ledger.regid Join public.remarks ON financialaccounting.ledger.remarks = public.remarks.remindex Where regno='8/05/546/100' ORDER BY financialaccounting.ledger.transdate DESC"
         );
-        $vars = get_object_vars($personResults[0]);
-        dd($vars);
+
+        $p = $personResults[0];
         // dd($amountResults);
-        return view('search-results', ['person' => $personResults, 'amounts' => $amountResults]);
+        return view('search-results', ['person' => $p, 'amounts' => $amountResults]);
     }
 }
--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -4,9 +4,46 @@
             <th>Particulars</th>
             <th>Value</th>
         </tr>
+
         <tr>
             <td>Reg. No.</td>
-            <td></td>
+            <td>{{$person->regno}}</td>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td>{{$person->name}}</td>
+        </tr>
+        <tr>
+            <td>Guardian Name</td>
+            <td>{{$person->guardianname}}</td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td>{{$person->empcuraddress}}</td>
+        </tr>
+        <tr>
+            <td>DOB</td>
+            <td>{{$person->dateofbirth}}</td>
+        </tr>
+        <tr>
+            <td>Start Date</td>
+            <td>{{$person->startingdate}}</td>
+        </tr>
+        <tr>
+            <td>Reg. Date</td>
+            <td>{{$person->regdate}}</td>
+        </tr>
+        <tr>
+            <td>Create Date</td>
+            <td>{{$person->createdate}}</td>
+        </tr>
+        <tr>
+            <td>ID Mark 1</td>
+            <td>{{$person->idmark1}}</td>
+        </tr>
+        <tr>
+            <td>ID Mark 2</td>
+            <td>{{$person->idmark2}}</td>
         </tr>
     </table>
 </div>
