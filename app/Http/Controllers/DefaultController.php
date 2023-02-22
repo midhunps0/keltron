5cc2ffed7e6d32afad547fae4873b9aaf92ed2af
midhunps <midhunps.0@gmail.com>
2023-02-22 16:27:56 +0530

    modified transactions search with left join

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -59,7 +59,7 @@ class DefaultController extends Controller
             "SELECT regno, empname, guardianname, empcuraddress, dateofbirth, startingdate, regdate, public.registration.createdate, idmark1, idmark2 FROM public.registration Where regno='$search'"
         );
         $amountResults = DB::select(
-            "SELECT financialaccounting.ledger.transdate, financialaccounting.ledger.vouchernum, financialaccounting.ledger.amount, public.remarks.remark FROM public.registration JOIN financialaccounting.ledger ON public.registration.regid = financialaccounting.ledger.regid Join public.remarks ON financialaccounting.ledger.remarks = public.remarks.remindex Where regno='$search' ORDER BY financialaccounting.ledger.transdate DESC"
+            "SELECT financialaccounting.ledger.transdate, financialaccounting.ledger.vouchernum, financialaccounting.ledger.amount, public.remarks.remark FROM public.registration JOIN financialaccounting.ledger ON public.registration.regid = financialaccounting.ledger.regid Left Join public.remarks ON financialaccounting.ledger.remarks = public.remarks.remindex Where regno='$search' ORDER BY financialaccounting.ledger.transdate DESC"
         );
 
         $allocanceResults = DB::select(
@@ -82,7 +82,7 @@ class DefaultController extends Controller
                 'persons' => $personResults,
                 'amounts' => $amountResults,
                 'allowances' => $allocanceResults,
-                ]
+            ]
         );
     }
 }
